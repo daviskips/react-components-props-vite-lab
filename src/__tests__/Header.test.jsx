15294894d75f5c9c64c657fd8628cbbd5af1db51
daviskiplangat <daviskiplangat74@gmail.com>
2026-08-17 13:18:36 +0300

    blog site

--- src/__tests__/Header.test.jsx
+++ src/__tests__/Header.test.jsx
@@ -1,15 +1,11 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Header from "../components/Header";
+function About(props) {
+  const imageSrc = props.image || "https://via.placeholder.com/215";
+  return (
+    <aside>
+      <img src={imageSrc} alt="blog logo" />
+      <p>{props.about}</p>
+    </aside>
+  );
+}
 
-test("renders a <header> element", () => {
-  const { container } = render(<Header />);
-  expect(container.querySelector("header")).toBeInTheDocument();
-});
-
-test("renders a <h1> with the blog name", () => {
-  render(<Header name="Underreacted" />);
-  const h1 = screen.queryByText("Underreacted");
-  expect(h1).toBeInTheDocument();
-  expect(h1.tagName).toBe("H1");
-});
+export default About;
